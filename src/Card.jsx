c3af87d9c1fb77cbe188b09fd3968f70a52fb1f4
kehindeDaniels <kehindedaniels501@gmail.com>
2024-02-08 22:22:15 +0100

    completed interractivity

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,12 +3,13 @@ import sport from './assets/sport.jpg'
 import star from './assets/star-icon.svg'
 
 export const Card = (props) => {
+  let count = props.rating < 5 ? 'sold out': 'Available';
   return (
     <>
       <div className="card">
-            <div className="card-img">
-                <img src={props.src} alt={props.name} />
-                
+            <div className="card-img relative">
+                <img src={props.src} alt={props.name} className='w-full'/>
+                <div className="absolute top-0 bg-slate-50 mt-1 ml-1 px-2 py-1 rounded-md ">{count}</div>
             </div>
 
             <div className="card-content">
